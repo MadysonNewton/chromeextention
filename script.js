0632952cd74e4715dc64ebaa4a4a6b2715a5bd61
Madyson Newton <mnewton.prsvr@gmail.com>
2024-06-12 19:49:33 -0500

    fixed errors

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById('ul-el')
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
-const leadsFromLocalStorage = JSon.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 console.log(leadsFromLocalStorage)
 
@@ -18,7 +18,7 @@ if (leadsFromLocalStorage) {
 tabBtn.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
-        myLeads.push(tab[0].url)
+        myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
 
